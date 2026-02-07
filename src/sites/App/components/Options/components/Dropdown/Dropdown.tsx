4818a380d283be0d02b944d054e4b7dde6a89d4e
Kolpixx <68741965+Kolpixx@users.noreply.github.com>
2026-02-07 22:11:19 +0100

    feat: prevent clicking other elements while dropdown is opened

--- src/sites/App/components/Options/components/Dropdown/Dropdown.tsx
+++ src/sites/App/components/Options/components/Dropdown/Dropdown.tsx
@@ -22,47 +22,52 @@ export default function Dropdown({ id, label, options, state, setState, variant,
     const [showingDropdown, showDropdown] = useState<boolean>(false);
 
     return (
-        <div className="dropdown-wrapper" style={{borderRadius: showingDropdown ? "10px 10px 0 0" : "10px"}} id={id}>
-            <div className="dropdown-label-wrapper pointer" onClick={() => showDropdown(!showingDropdown)}>
-                <span className="dropdown-label">{label}</span>
-            </div>
+        <>
             {showingDropdown &&
-                <>
-                    <div className="dropdown-options">
-                        {Array.from(options).map((option, index) => {
-                            return (
-                                <div className="dropdown-option" key={index}>
-                                    {(variant === "checkbox" && state instanceof Array) &&
-                                        <>
-                                            <Checkbox
-                                                id={option[0]}
-                                                state={state}
-                                                setState={setState}
-                                            />
-                                            <span>{option[1]}</span>
-                                        </>
-                                    }
-                                    {(variant === "sort" || variant === "radio" && typeof state === "string") &&
-                                        <button className={option[0] === sorting ? "dropdown-radio-active" : ""} onClick={() => setState(option[0])}>
-                                            {option[1]}
-                                        </button>
-                                    }
-                                </div>
-                            )
-                        })}
-                        {(variant === "sort" && setSortingOrder) &&
-                            <>
-                                <div className="divider" />
-                                <div className="dropdown-sort-order">
-                                    <button className={sortingOrder === "ascending" ? "dropdown-radio-active" : ""} onClick={() => setSortingOrder("ascending")}>Ascending</button>
-                                    <button className={sortingOrder === "descending" ? "dropdown-radio-active" : ""} onClick={() => setSortingOrder("descending")}>Descending</button>
-                                </div>
-                            </>
-                        }
-                    </div>
-                </>
-                
+                <div className="dropdown-background" onClick={((e) => {e.stopPropagation(); showDropdown(false)})}></div>
             }
-        </div>
+            <div className="dropdown-wrapper" style={{borderRadius: showingDropdown ? "10px 10px 0 0" : "10px", zIndex: showingDropdown ? "2" : "0"}} id={id}>
+                <div className="dropdown-label-wrapper pointer" onClick={() => showDropdown(!showingDropdown)}>
+                    <span className="dropdown-label">{label}</span>
+                </div>
+                {showingDropdown &&
+                    <>
+                        <div className="dropdown-options">
+                            {Array.from(options).map((option, index) => {
+                                return (
+                                    <div className="dropdown-option" key={index}>
+                                        {(variant === "checkbox" && state instanceof Array) &&
+                                            <>
+                                                <Checkbox
+                                                    id={option[0]}
+                                                    state={state}
+                                                    setState={setState}
+                                                />
+                                                <span>{option[1]}</span>
+                                            </>
+                                        }
+                                        {(variant === "sort" || variant === "radio" && typeof state === "string") &&
+                                            <button className={option[0] === sorting ? "dropdown-radio-active" : ""} onClick={() => setState(option[0])}>
+                                                {option[1]}
+                                            </button>
+                                        }
+                                    </div>
+                                )
+                            })}
+                            {(variant === "sort" && setSortingOrder) &&
+                                <>
+                                    <div className="divider" />
+                                    <div className="dropdown-sort-order">
+                                        <button className={sortingOrder === "ascending" ? "dropdown-radio-active" : ""} onClick={() => setSortingOrder("ascending")}>Ascending</button>
+                                        <button className={sortingOrder === "descending" ? "dropdown-radio-active" : ""} onClick={() => setSortingOrder("descending")}>Descending</button>
+                                    </div>
+                                </>
+                            }
+                        </div>
+                    </>
+                    
+                }
+            </div>
+        </>
     )
 }
